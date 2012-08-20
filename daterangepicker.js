7d8247881bf7d518fb232d10c67d624cd73a9022
Dan Grossman <dan@awio.com>
2012-08-20 01:31:46 -0300

    Added license information

--- daterangepicker.js
+++ daterangepicker.js
@@ -1,3 +1,11 @@
+/**
+ * @version: 1.0
+ * @author: Dan Grossman http://www.dangrossman.info/
+ * @date: 2012-08-20
+ * @copyright: Copyright (c) 2012 Dan Grossman. All rights reserved.
+ * @license: Licensed under Apache License v2.0. See http://www.apache.org/licenses/LICENSE-2.0
+ * @website: http://www.improvely.com/
+ */
 !function($) {
   
   var DateRangePicker = function(element, options, cb) {
