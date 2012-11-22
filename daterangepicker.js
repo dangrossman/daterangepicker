4683ed1a7765ced6da4782d6692f2a5e186e483d
VINAY Kr. SHARMA <sharma_salient@hotmail.com>
2012-11-22 18:20:29 +0530

    Update daterangepicker.js
    daysOfWeek and monthNames got from Date.CultureInfo

--- daterangepicker.js
+++ daterangepicker.js
@@ -27,8 +27,8 @@
             fromLabel:"From",
             toLabel:"To",
             customRangeLabel:"Custom Range",
-            daysOfWeek:['Su', 'Mo', 'Tu', 'We', 'Th', 'Fr','Sa'],
-            monthNames:['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'],
+            daysOfWeek:Date.CultureInfo.shortestDayNames,
+            monthNames:Date.CultureInfo.monthNames,
             firstDay:0
         };
 
