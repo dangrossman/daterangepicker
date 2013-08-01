1398eb58813cfe1138fda6f08a70d0c2ca6a7ac3
Matthew Hailwood <me@matthewhailwood.co.nz>
2013-08-02 00:39:53 +1200

    Add autoShow option to always show the calendars.

--- daterangepicker.js
+++ daterangepicker.js
@@ -246,7 +246,8 @@
             right.removeClass('right').addClass('left');
         }
 
-        if (typeof options == 'undefined' || typeof options.ranges == 'undefined') {
+        if (typeof options == 'undefined' || typeof options.ranges == 'undefined' 
+        || (typeof options.autoShow == 'boolean' && options.autoShow)) {
             this.container.find('.calendar').show();
             this.move();
         }
