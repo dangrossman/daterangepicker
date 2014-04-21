e9544f9146fd31faf1067a68c49b24acc314c223
Narendra <narendra@narendrasisodiya.com>
2014-04-21 17:19:51 +0530

    Added boolean parameter alwaysShowCalendar to give a visual feedback for selected dateRange

--- daterangepicker.js
+++ daterangepicker.js
@@ -105,6 +105,7 @@
 
             this.showDropdowns = false;
             this.showWeekNumbers = false;
+            this.alwaysShowCalendar = false;
             this.timePicker = false;
             this.timePickerIncrement = 30;
             this.timePicker12Hour = true;
@@ -227,6 +228,10 @@
 
             if (typeof options.showWeekNumbers === 'boolean') {
                 this.showWeekNumbers = options.showWeekNumbers;
+            }
+
+            if (typeof options.alwaysShowCalendar === 'boolean') {
+                this.alwaysShowCalendar = options.alwaysShowCalendar;
             }
 
             if (typeof options.buttonClasses === 'string') {
@@ -566,6 +571,9 @@
             if (label == this.locale.customRangeLabel) {
                 this.showCalendars();
             } else {
+                if(this.alwaysShowCalendar){
+                    this.showCalendars();
+                }
                 var dates = this.ranges[label];
 
                 this.startDate = dates[0];
@@ -582,8 +590,11 @@
 
                 this.updateInputText();
 
-                this.hideCalendars();
-                this.hide();
+                if(!this.alwaysShowCalendar){
+                    this.hideCalendars();
+                    this.hide();
+                }
+                
                 this.element.trigger('apply.daterangepicker', this);
             }
         },
