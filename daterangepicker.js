481e632b5e15de16f6a54f90a8c2f32b4ebfc843
Aleksandr Savchenko <aleksandr.s@giantleaplab.com>
2014-08-28 17:16:07 +0300

    removed unused variable

--- daterangepicker.js
+++ daterangepicker.js
@@ -41,26 +41,6 @@
         //element that triggered the date range picker
         this.element = $(element);
 
-        //create the picker HTML object
-        var DRPTemplate = '<div class="daterangepicker dropdown-menu">' +
-                '<div class="calendar left"></div>' +
-                '<div class="calendar right"></div>' +
-                '<div class="ranges">' +
-                  '<div class="range_inputs">' +
-                    '<div class="daterangepicker_start_input">' +
-                      '<label for="daterangepicker_start"></label>' +
-                      '<input class="input-mini" type="text" name="daterangepicker_start" value="" disabled="disabled" />' +
-                    '</div>' +
-                    '<div class="daterangepicker_end_input">' +
-                      '<label for="daterangepicker_end"></label>' +
-                      '<input class="input-mini" type="text" name="daterangepicker_end" value="" disabled="disabled" />' +
-                    '</div>' +
-                    '<button class="applyBtn" disabled="disabled"></button>&nbsp;' +
-                    '<button class="cancelBtn"></button>' +
-                  '</div>' +
-                '</div>' +
-              '</div>';
-
         //custom options
         if (typeof options !== 'object' || options === null)
             options = {};
@@ -160,22 +140,22 @@
 
             this.template =
                 '<div class="daterangepicker dropdown-menu">' +
-                '<div class="calendar left"></div>' +
-                '<div class="calendar right"></div>' +
-                '<div class="ranges">' +
-                '<div class="range_inputs">' +
-                '<div class="daterangepicker_start_input">' +
-                '<label for="daterangepicker_start"></label>' +
-                '<input class="input-mini" type="text" name="daterangepicker_start" value="" disabled="disabled" />' +
-                '</div>' +
-                '<div class="daterangepicker_end_input">' +
-                '<label for="daterangepicker_end"></label>' +
-                '<input class="input-mini" type="text" name="daterangepicker_end" value="" disabled="disabled" />' +
-                '</div>' +
-                '<button class="applyBtn" disabled="disabled"></button>&nbsp;' +
-                '<button class="cancelBtn"></button>' +
-                '</div>' +
-                '</div>' +
+                    '<div class="calendar left"></div>' +
+                    '<div class="calendar right"></div>' +
+                    '<div class="ranges">' +
+                        '<div class="range_inputs">' +
+                            '<div class="daterangepicker_start_input">' +
+                                '<label for="daterangepicker_start"></label>' +
+                                '<input class="input-mini" type="text" name="daterangepicker_start" value="" disabled="disabled" />' +
+                            '</div>' +
+                            '<div class="daterangepicker_end_input">' +
+                                '<label for="daterangepicker_end"></label>' +
+                                '<input class="input-mini" type="text" name="daterangepicker_end" value="" disabled="disabled" />' +
+                            '</div>' +
+                            '<button class="applyBtn" disabled="disabled"></button>&nbsp;' +
+                            '<button class="cancelBtn"></button>' +
+                        '</div>' +
+                    '</div>' +
                 '</div>';
 
             if (typeof options.template == 'string')
