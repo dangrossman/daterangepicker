a7b9ccdaf3add1aa96682f004f567ff6e8b7ae63
Dan Grossman <dan@awio.com>
2015-07-16 09:33:04 -0400

    Update moment.js

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'dangrossman:bootstrap-daterangepicker',
-  version: '2.0.0',
+  version: '2.0.1',
   summary: 'Date range picker component for Bootstrap',
   git: 'https://github.com/dangrossman/bootstrap-daterangepicker',
   documentation: 'README.md'
