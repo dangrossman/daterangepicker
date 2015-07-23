12fdbc0a4b3e4173eaccd4e21f19c2a36fe8a25a
Dan Grossman <dan@awio.com>
2015-07-23 08:57:22 -0400

    2.0.4

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'dangrossman:bootstrap-daterangepicker',
-  version: '2.0.3',
+  version: '2.0.4',
   summary: 'Date range picker component for Bootstrap',
   git: 'https://github.com/dangrossman/bootstrap-daterangepicker',
   documentation: 'README.md'
