90948a844c3c5e380b3435b79aa3e4985b7e2b2c
Dan Grossman <dan@awio.com>
2015-10-08 10:09:59 -0400

    Up version number to fix a CDN issue; no other changes

--- daterangepicker.js
+++ daterangepicker.js
@@ -1,5 +1,5 @@
 /**
-* @version: 2.0.14
+* @version: 2.1.12
 * @author: Dan Grossman http://www.dangrossman.info/
 * @copyright: Copyright (c) 2012-2015 Dan Grossman. All rights reserved.
 * @license: Licensed under the MIT license. See http://www.opensource.org/licenses/mit-license.php
--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'dangrossman:bootstrap-daterangepicker',
-  version: '2.0.14',
+  version: '2.1.12',
   summary: 'Date range picker component for Bootstrap',
   git: 'https://github.com/dangrossman/bootstrap-daterangepicker',
   documentation: 'README.md'
