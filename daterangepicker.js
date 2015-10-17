88416039fa31b6fa22a7ef37b18a984942261d7a
Dan Grossman <dan@awio.com>
2015-10-17 10:59:15 -0400

    Bump version

--- daterangepicker.js
+++ daterangepicker.js
@@ -1,5 +1,5 @@
 /**
-* @version: 2.1.12
+* @version: 2.1.13
 * @author: Dan Grossman http://www.dangrossman.info/
 * @copyright: Copyright (c) 2012-2015 Dan Grossman. All rights reserved.
 * @license: Licensed under the MIT license. See http://www.opensource.org/licenses/mit-license.php
--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'dangrossman:bootstrap-daterangepicker',
-  version: '2.0.14',
+  version: '2.1.13',
   summary: 'Date range picker component for Bootstrap',
   git: 'https://github.com/dangrossman/bootstrap-daterangepicker',
   documentation: 'README.md'
