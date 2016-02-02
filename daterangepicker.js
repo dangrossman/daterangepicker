73db3eeba88aa1dacbbf9c25105335dd980a4906
Raul Popadineti <raulpopadineti@gmail.com>
2016-02-02 12:13:56 +0200

    Start and end date should update accordingly on formInputsChanged

--- daterangepicker.js
+++ daterangepicker.js
@@ -1446,9 +1446,9 @@
                 this.setEndDate(end);
 
                 if (isRight) {
-                    this.container.find('input[name="daterangepicker_start"]').val(this.startDate.format(this.locale.format));
-                } else {
                     this.container.find('input[name="daterangepicker_end"]').val(this.endDate.format(this.locale.format));
+                } else {
+                    this.container.find('input[name="daterangepicker_start"]').val(this.startDate.format(this.locale.format));
                 }
 
             }
