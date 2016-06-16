4dee09abac30325c1ccf1a725666a0264ec4f6f0
Dan Grossman <dan@awio.com>
2016-06-16 13:42:08 -0400

    Experimental: Clicking on the right-hand text input in the UI allows re-selection of the end of the current date range without first re-selecting the beginning on the calendar

--- daterangepicker.js
+++ daterangepicker.js
@@ -1,5 +1,5 @@
 /**
-* @version: 2.1.22
+* @version: 2.1.23-beta
 * @author: Dan Grossman http://www.dangrossman.info/
 * @copyright: Copyright (c) 2012-2016 Dan Grossman. All rights reserved.
 * @license: Licensed under the MIT license. See http://www.opensource.org/licenses/mit-license.php
@@ -1496,8 +1496,21 @@
 
         formInputsFocused: function(e) {
 
+            // Highlight the focused input
             this.container.find('input[name="daterangepicker_start"], input[name="daterangepicker_end"]').removeClass('active');
             $(e.target).addClass('active');
+
+            // Set the state such that if the user goes back to using a mouse, 
+            // the calendars are aware we're selecting the end of the range, not
+            // the start. This allows someone to edit the end of a date range without
+            // re-selecting the beginning, by clicking on the end date input then
+            // using the calendar.
+            var isRight = $(e.target).closest('.calendar').hasClass('right');
+            if (isRight) {
+                this.endDate = null;
+                this.setStartDate(this.startDate.clone());
+                this.updateView();
+            }
 
         },
 
