55b77d2b7563fffdd4d72e31b78aae3ba14c0507
Scott Fan <fancp2007@gmail.com>
2016-10-15 12:02:24 +0800

    Fix: the end of the range cannot before the start

--- daterangepicker.js
+++ daterangepicker.js
@@ -325,9 +325,11 @@
                     end = maxDate.clone();
 
                 // If the end of the range is before the minimum or the start of the range is
-                // after the maximum, don't display this range option at all.
+                // after the maximum or the end of the range is before the start of the range,
+                // don't display this range option at all.
                 if ((this.minDate && end.isBefore(this.minDate, this.timepicker ? 'minute' : 'day')) 
-                  || (maxDate && start.isAfter(maxDate, this.timepicker ? 'minute' : 'day')))
+                  || (maxDate && start.isAfter(maxDate, this.timepicker ? 'minute' : 'day'))
+                  || (end.isBefore(start)))
                     continue;
 
                 //Support unicode chars in the range names.
