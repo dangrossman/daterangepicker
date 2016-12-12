2c101a06516fa2139b96e35803fa067ac2ff39e4
Erik Uetz <erik.uetz@humach.com>
2016-12-12 09:47:07 -0600

    updated styles

--- daterangepicker.js
+++ daterangepicker.js
@@ -4,8 +4,6 @@
 * @copyright: Copyright (c) 2012-2016 Dan Grossman. All rights reserved.
 * @license: Licensed under the MIT license. See http://www.opensource.org/licenses/mit-license.php
 * @website: https://www.improvely.com/
-*
-* forked and modified by Erik Uetz
 */
 // Follow the UMD template https://github.com/umdjs/umd/blob/master/templates/returnExportsGlobal.js
 (function (root, factory) {
