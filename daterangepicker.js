dcb1af55f721ca974e6ae56dea54447b43ce1a7b
Dan Grossman <dan@awio.com>
2017-02-06 10:00:29 -0500

    Fix link in doc

--- daterangepicker.js
+++ daterangepicker.js
@@ -3,7 +3,7 @@
 * @author: Dan Grossman http://www.dangrossman.info/
 * @copyright: Copyright (c) 2012-2017 Dan Grossman. All rights reserved.
 * @license: Licensed under the MIT license. See http://www.opensource.org/licenses/mit-license.php
-* @website: https://www.daterangepicker.com/
+* @website: http://www.daterangepicker.com/
 */
 // Follow the UMD template https://github.com/umdjs/umd/blob/master/templates/returnExportsGlobal.js
 (function (root, factory) {
