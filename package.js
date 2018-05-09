fba7dc416be6b38a6b2051cf6bdc84920f8cd6f7
Dan <dan@awio.com>
2018-05-09 16:25:45 -0400

    Bump version, add missing docs

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'dangrossman:bootstrap-daterangepicker',
-  version: '3.0.1',
+  version: '3.0.2',
   summary: 'Date range picker component',
   git: 'https://github.com/dangrossman/daterangepicker',
   documentation: 'README.md'
