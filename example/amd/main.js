1986a1d6103ec5810c9afddf90ad8f908049c80c
Aurelie <gramusset.a@gmail.com>
2020-04-06 16:15:16 +0200

    revert automatique formatting

--- example/amd/main.js
+++ example/amd/main.js
@@ -1,154 +1,152 @@
 requirejs.config({
     "paths": {
-        "jquery": "https://code.jquery.com/jquery-1.11.3.min",
-        "moment": "../../moment",
-        "daterangepicker": "../../daterangepicker"
+      "jquery": "https://code.jquery.com/jquery-1.11.3.min",
+      "moment": "../../moment",
+      "daterangepicker": "../../daterangepicker"
     }
 });
 
-requirejs(['jquery', 'moment', 'daterangepicker'], function ($, moment) {
-    $(document).ready(function () {
+requirejs(['jquery', 'moment', 'daterangepicker'] , function ($, moment) {
+$(document).ready(function() {
 
-        $('#config-text').keyup(function () {
-            eval($(this).val());
+  $('#config-text').keyup(function() {
+    eval($(this).val());
+  });
+
+  $('.configurator input, .configurator select').change(function() {
+    updateConfig();
+  });
+
+  $('.demo i').click(function() {
+    $(this).parent().find('input').click();
+  });
+
+  $('#startDate').daterangepicker({
+    singleDatePicker: true,
+    startDate: moment().subtract(6, 'days')
+  });
+
+  $('#endDate').daterangepicker({
+    singleDatePicker: true,
+    startDate: moment()
+  });
+
+  updateConfig();
+
+  function updateConfig() {
+    var options = {};
+
+    if ($('#singleDatePicker').is(':checked'))
+      options.singleDatePicker = true;
+
+    if ($('#showDropdowns').is(':checked'))
+      options.showDropdowns = true;
+
+    if ($('#showWeekNumbers').is(':checked'))
+      options.showWeekNumbers = true;
+
+    if ($('#showISOWeekNumbers').is(':checked'))
+      options.showISOWeekNumbers = true;
+
+    if ($('#timePicker').is(':checked'))
+      options.timePicker = true;
+
+    if ($('#timePicker24Hour').is(':checked'))
+        options.timePicker24Hour = true;
+
+    if ($('#timePickerHours').val().length) {
+        const hoursString = $('#timePickerHours').val();
+        const hours = hoursString.split(',');
+        options.timePickerHours = hours.map(function (hour) {
+            return parseInt(hour, 10)
+        }).filter(function (hour) {
+            return hour >= 0 && hour <= 23
         });
+        options.timePicker24Hour = options.timePickerHours.length > 0 ? true : options.timePicker24Hour;
+    }
 
-        $('.configurator input, .configurator select').change(function () {
-            updateConfig();
-        });
+    if ($('#timePickerIncrement').val().length && $('#timePickerIncrement').val() != 1)
+      options.timePickerIncrement = parseInt($('#timePickerIncrement').val(), 10);
 
-        $('.demo i').click(function () {
-            $(this).parent().find('input').click();
-        });
+    if ($('#timePickerSeconds').is(':checked'))
+      options.timePickerSeconds = true;
 
-        $('#startDate').daterangepicker({
-            singleDatePicker: true,
-            startDate: moment().subtract(6, 'days')
-        });
+    if ($('#autoApply').is(':checked'))
+      options.autoApply = true;
 
-        $('#endDate').daterangepicker({
-            singleDatePicker: true,
-            startDate: moment()
-        });
+    if ($('#dateLimit').is(':checked'))
+      options.dateLimit = { days: 7 };
 
-        updateConfig();
+    if ($('#ranges').is(':checked')) {
+      options.ranges = {
+        'Today': [moment(), moment()],
+        'Yesterday': [moment().subtract(1, 'days'), moment().subtract(1, 'days')],
+        'Last 7 Days': [moment().subtract(6, 'days'), moment()],
+        'Last 30 Days': [moment().subtract(29, 'days'), moment()],
+        'This Month': [moment().startOf('month'), moment().endOf('month')],
+        'Last Month': [moment().subtract(1, 'month').startOf('month'), moment().subtract(1, 'month').endOf('month')]
+      };
+    }
 
-        function updateConfig() {
-            var options = {};
+    if ($('#locale').is(':checked')) {
+      options.locale = {
+        format: 'MM/DD/YYYY HH:mm',
+        separator: ' - ',
+        applyLabel: 'Apply',
+        cancelLabel: 'Cancel',
+        fromLabel: 'From',
+        toLabel: 'To',
+        customRangeLabel: 'Custom',
+        daysOfWeek: ['Su', 'Mo', 'Tu', 'We', 'Th', 'Fr','Sa'],
+        monthNames: ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'],
+        firstDay: 1
+      };
+    }
 
-            if ($('#singleDatePicker').is(':checked'))
-                options.singleDatePicker = true;
+    if (!$('#linkedCalendars').is(':checked'))
+      options.linkedCalendars = false;
 
-            if ($('#showDropdowns').is(':checked'))
-                options.showDropdowns = true;
+    if (!$('#autoUpdateInput').is(':checked'))
+      options.autoUpdateInput = false;
 
-            if ($('#showWeekNumbers').is(':checked'))
-                options.showWeekNumbers = true;
+    if ($('#alwaysShowCalendars').is(':checked'))
+      options.alwaysShowCalendars = true;
 
-            if ($('#showISOWeekNumbers').is(':checked'))
-                options.showISOWeekNumbers = true;
+    if ($('#parentEl').val().length)
+      options.parentEl = $('#parentEl').val();
 
-            if ($('#timePicker').is(':checked'))
-                options.timePicker = true;
+    if ($('#startDate').val().length)
+      options.startDate = $('#startDate').val();
 
-            if ($('#timePicker24Hour').is(':checked'))
-                options.timePicker24Hour = true;
+    if ($('#endDate').val().length)
+      options.endDate = $('#endDate').val();
 
-            if ($('#timePickerHours').val().length) {
-                const hoursString = $('#timePickerHours').val();
-                const hours = hoursString.split(',');
-                options.timePickerHours = hours.map(function (hour) {
-                    return parseInt(hour, 10)
-                }).filter(function (hour) {
-                    return hour >= 0 && hour <= 23
-                });
-                options.timePicker24Hour = options.timePickerHours.length > 0 ? true : options.timePicker24Hour;
-            }
+    if ($('#minDate').val().length)
+      options.minDate = $('#minDate').val();
 
-            if ($('#timePickerIncrement').val().length && $('#timePickerIncrement').val() != 1)
-                options.timePickerIncrement = parseInt($('#timePickerIncrement').val(), 10);
+    if ($('#maxDate').val().length)
+      options.maxDate = $('#maxDate').val();
 
-            if ($('#timePickerSeconds').is(':checked'))
-                options.timePickerSeconds = true;
+    if ($('#opens').val().length && $('#opens').val() != 'right')
+      options.opens = $('#opens').val();
 
-            if ($('#autoApply').is(':checked'))
-                options.autoApply = true;
+    if ($('#drops').val().length && $('#drops').val() != 'down')
+      options.drops = $('#drops').val();
 
-            if ($('#dateLimit').is(':checked'))
-                options.dateLimit = {days: 7};
+    if ($('#buttonClasses').val().length && $('#buttonClasses').val() != 'btn btn-sm')
+      options.buttonClasses = $('#buttonClasses').val();
 
-            if ($('#ranges').is(':checked')) {
-                options.ranges = {
-                    'Today': [moment(), moment()],
-                    'Yesterday': [moment().subtract(1, 'days'), moment().subtract(1, 'days')],
-                    'Last 7 Days': [moment().subtract(6, 'days'), moment()],
-                    'Last 30 Days': [moment().subtract(29, 'days'), moment()],
-                    'This Month': [moment().startOf('month'), moment().endOf('month')],
-                    'Last Month': [moment().subtract(1, 'month').startOf('month'), moment().subtract(1, 'month').endOf('month')]
-                };
-            }
+    if ($('#applyClass').val().length && $('#applyClass').val() != 'btn-success')
+      options.applyClass = $('#applyClass').val();
 
-            if ($('#locale').is(':checked')) {
-                options.locale = {
-                    format: 'MM/DD/YYYY HH:mm',
-                    separator: ' - ',
-                    applyLabel: 'Apply',
-                    cancelLabel: 'Cancel',
-                    fromLabel: 'From',
-                    toLabel: 'To',
-                    customRangeLabel: 'Custom',
-                    daysOfWeek: ['Su', 'Mo', 'Tu', 'We', 'Th', 'Fr', 'Sa'],
-                    monthNames: ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'],
-                    firstDay: 1
-                };
-            }
+    if ($('#cancelClass').val().length && $('#cancelClass').val() != 'btn-default')
+      options.cancelClass = $('#cancelClass').val();
 
-            if (!$('#linkedCalendars').is(':checked'))
-                options.linkedCalendars = false;
+    $('#config-text').val("$('#demo').daterangepicker(" + JSON.stringify(options, null, '    ') + ", function(start, end, label) {\n  console.log(\"New date range selected: ' + start.format('YYYY-MM-DD') + ' to ' + end.format('YYYY-MM-DD') + ' (predefined range: ' + label + ')\");\n});");
 
-            if (!$('#autoUpdateInput').is(':checked'))
-                options.autoUpdateInput = false;
+    $('#config-demo').daterangepicker(options, function(start, end, label) { console.log('New date range selected: ' + start.format('YYYY-MM-DD') + ' to ' + end.format('YYYY-MM-DD') + ' (predefined range: ' + label + ')'); });
 
-            if ($('#alwaysShowCalendars').is(':checked'))
-                options.alwaysShowCalendars = true;
+  }
 
-            if ($('#parentEl').val().length)
-                options.parentEl = $('#parentEl').val();
-
-            if ($('#startDate').val().length)
-                options.startDate = $('#startDate').val();
-
-            if ($('#endDate').val().length)
-                options.endDate = $('#endDate').val();
-
-            if ($('#minDate').val().length)
-                options.minDate = $('#minDate').val();
-
-            if ($('#maxDate').val().length)
-                options.maxDate = $('#maxDate').val();
-
-            if ($('#opens').val().length && $('#opens').val() != 'right')
-                options.opens = $('#opens').val();
-
-            if ($('#drops').val().length && $('#drops').val() != 'down')
-                options.drops = $('#drops').val();
-
-            if ($('#buttonClasses').val().length && $('#buttonClasses').val() != 'btn btn-sm')
-                options.buttonClasses = $('#buttonClasses').val();
-
-            if ($('#applyClass').val().length && $('#applyClass').val() != 'btn-success')
-                options.applyClass = $('#applyClass').val();
-
-            if ($('#cancelClass').val().length && $('#cancelClass').val() != 'btn-default')
-                options.cancelClass = $('#cancelClass').val();
-
-            $('#config-text').val("$('#demo').daterangepicker(" + JSON.stringify(options, null, '    ') + ", function(start, end, label) {\n  console.log(\"New date range selected: ' + start.format('YYYY-MM-DD') + ' to ' + end.format('YYYY-MM-DD') + ' (predefined range: ' + label + ')\");\n});");
-
-            $('#config-demo').daterangepicker(options, function (start, end, label) {
-                console.log('New date range selected: ' + start.format('YYYY-MM-DD') + ' to ' + end.format('YYYY-MM-DD') + ' (predefined range: ' + label + ')');
-            });
-
-        }
-
-    });
+});
 });
