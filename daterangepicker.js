1f38850c47ffaec46dfd40fe052a66779588cc55
Artem Gurzhii <artem.gurzhii@gmail.com>
2020-05-14 15:35:23 +0300

    drop everything

--- daterangepicker.js
+++ daterangepicker.js
@@ -7,29 +7,7 @@
 */
 // Following the UMD template https://github.com/umdjs/umd/blob/master/templates/returnExportsGlobal.js
 
-(function (root, factory) {
-  if (typeof define === 'function' && define.amd) {
-      // AMD. Make globaly available as well
-      define(['moment', 'jquery'], function (moment, jquery) {
-          if (!jquery.fn) jquery.fn = {}; // webpack server rendering
-          if (typeof moment !== 'function' && moment.default) moment = moment.default
-          return factory(moment, jquery);
-      });
-  } else if (typeof module === 'object' && module.exports) {
-      // Node / Browserify
-      //isomorphic issue
-      var jQuery = (typeof window != 'undefined') ? window.jQuery : undefined;
-      if (!jQuery) {
-          jQuery = require('jquery');
-          if (!jQuery.fn) jQuery.fn = {};
-      }
-      var moment = (typeof window != 'undefined' && typeof window.moment != 'undefined') ? window.moment : require('moment');
-      module.exports = factory(moment, jQuery);
-  } else {
-      // Browser globals
-      root.daterangepicker = factory(root.moment, root.jQuery);
-  }
-}(this, function(moment, $) {
+!(function(moment, $) {
   var DateRangePicker = function(element, options, cb) {
 
       //default settings for options
@@ -1672,4 +1650,4 @@
 
   return DateRangePicker;
 
-}));
+})(window.moment, window.$);
