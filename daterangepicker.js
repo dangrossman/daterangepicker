b4e7f9a8b210bd26d507a63676f7c99e7509ded5
Mgo <mgotm13@gmail.com>
2021-02-19 21:41:41 +0400

    Caluculate Chosen Label code modifications

--- daterangepicker.js
+++ daterangepicker.js
@@ -1374,14 +1374,12 @@
                     //ignore times when comparing dates if time picker seconds is not enabled
                     if (this.startDate.format(format) == this.ranges[range][0].format(format) && this.endDate.format(format) == this.ranges[range][1].format(format)) {
                         customRange = false;
-                        this.chosenLabel = this.container.find('.ranges li:eq(' + i + ')').addClass('active').attr('data-range-key');
                         break;
                     }
                 } else {
                     //ignore times when comparing dates if time picker is not enabled
                     if (this.startDate.format('YYYY-MM-DD') == this.ranges[range][0].format('YYYY-MM-DD') && this.endDate.format('YYYY-MM-DD') == this.ranges[range][1].format('YYYY-MM-DD')) {
                         customRange = false;
-                        this.chosenLabel = this.container.find('.ranges li:eq(' + i + ')').addClass('active').attr('data-range-key');
                         break;
                     }
                 }
@@ -1394,6 +1392,9 @@
                     this.chosenLabel = null;
                 }
                 this.showCalendars();
+            } else {
+                // activate one the the chosen labels
+                this.chosenLabel = this.container.find('.ranges li:eq(' + i + ')').addClass('active').attr('data-range-key');
             }
         },
 
